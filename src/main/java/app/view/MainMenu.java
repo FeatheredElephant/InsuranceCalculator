819491eac02ef9db6e306a6fa15f24511a6ad034
Elaman Kazakbaev <96164456+kazakbaevelaman@users.noreply.github.com>
2023-02-10 18:21:43 -0600

    main menu refactored

--- src/main/java/app/view/MainMenu.java
+++ src/main/java/app/view/MainMenu.java
@@ -1,41 +1,32 @@
 package app.view;
-
-
 import app.IOManager;
-
 import java.util.Scanner;
 
 public class MainMenu {
-    public static void main(String[] args) {
 
-        IOManager io = IOManager.getInstance();
-        Scanner scanner = new Scanner(System.in);
-        CustomerMenu customerMenu;
-        AgentMenu agentMenu;
+    private static final IOManager io = IOManager.getInstance();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void displayMenu(String userType) {
+        IMenu menu;
         String option;
         boolean isExit;
-        String user = "customer";
 
-        switch (user) {
-            case "customer":
-                customerMenu = new CustomerMenu();
-                do {
-                    customerMenu.view();
-                    option = scanner.next();
-                    isExit = customerMenu.selectOption(option);
-                } while (isExit);
-                io.println("User exits application.");
-                break;
-            case "agent":
-                agentMenu = new AgentMenu();
-                do {
-                    agentMenu.view();
-                    option = scanner.next();
-                    isExit = agentMenu.selectOption(option);
-                } while (isExit);
-                io.println("User exits application.");
+        if (userType.equals("customer")) {
+            menu = new CustomerMenu();
+        } else if (userType.equals("agent")) {
+            menu = new AgentMenu();
+        } else {
+            io.println("Invalid user type.");
+            return;
         }
 
-
+        do {
+            menu.view();
+            option = scanner.next();
+            isExit = menu.selectOption(option);
+        } while (isExit);
+        io.println("User exits application.");
     }
+
 }
